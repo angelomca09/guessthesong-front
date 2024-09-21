dde7fbeb2df883b41a4f5cc453c2436ffce18673
Angelo Munhoz <angelomca09@gmail.com>
2024-09-21 13:58:06 -0300

    Clarify the API response

--- server/python.py
+++ server/python.py
@@ -7,28 +7,39 @@ def main(playlist_id):
     tam = 70
     url = f"https://api.deezer.com/playlist/{playlist_id}/tracks?limit={tam}"
     response = requests.get(url)
-    tracks = response.json()["data"]
-    preview=[]
-    previewUrls=[]
-    while(len(preview)<numSongs):
-        diff = numSongs-len(preview)
-        Aux = random.choices(tracks,k=diff)
-        # Step 2: Retrieve the preview URL for each track
-        for track in Aux:
-            preview_track = {}
-            preview_track["title"] = track["title"]
-            preview_track["album"] = track["album"]["title"]
-            preview_track["cover"] = track["album"]["cover_medium"]
-            preview_track["artist"] = track["artist"]["name"]
+    playlist_tracks = response.json()["data"]
+    tracks=[]
+    previews=[]
 
-            if(track["preview"] !="" and not(track["preview"] in previewUrls)):
-                previewUrls.append(track["preview"])
-                preview.append(preview_track)
-    Aux = random.choice(previewUrls)
-    preview.append(Aux)
-    preview.append(previewUrls.index(Aux))
-    # Print the previews
-    return preview
+    # Step 2: Fill the track list with random songs from the Playlist
+    while(len(tracks)<numSongs):
+        diff = numSongs-len(tracks)
+        random_tracks = random.choices(playlist_tracks,k=diff)
+        # Step 2.1: Retrieve information for each track
+        for track in random_tracks:
+            new_track = {}
+            new_track["title"] = track["title"]
+            new_track["album"] = track["album"]["title"]
+            new_track["cover"] = track["album"]["cover_medium"]
+            new_track["artist"] = track["artist"]["name"]
+
+            # Step 2.2: Save track if not repeated and store it preview separately for later
+            if(track["preview"] !="" and not(track["preview"] in previews)):
+                previews.append(track["preview"])
+                tracks.append(new_track)
+
+    # Step 3: Select a random preview
+    selected_preview = random.choice(previews)
+
+    # Step 4: Build the final object
+
+    obj = {}
+
+    obj["answer"] = previews.index(selected_preview)
+    obj["preview"] = selected_preview
+    obj["tracks"] = tracks
+
+    return obj
 
 if __name__ == '__main__':
     # Call the main function with a default playlist ID (optional)
