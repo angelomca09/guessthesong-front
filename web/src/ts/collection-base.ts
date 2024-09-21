d99cf634f99e1d2be8015c73059ab3f83f131745
Angelo Munhoz <angelomca09@gmail.com>
2024-09-21 12:13:15 -0300

    Add Collections Base

---
+++ web/src/ts/collection-base.ts
@@ -0,0 +1,12 @@
+function getCollections() {
+    let collections = [
+        {
+            name: "Games",
+            playlistId: "12645052643"
+        }
+    ]
+
+    // Add the collections from localStorage
+
+    return collections;
+}
