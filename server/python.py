d2f43699611b510c8d2d36dcbc7b04a5c781aca2
josermca01 <josermca01@gmail.com>
2024-09-21 13:28:50 -0300

    Change API return

--- server/python.py
+++ server/python.py
@@ -3,27 +3,30 @@ import random
 
 def main(playlist_id):
     # Step 1: Retrieve the tracks in the playlist
+    numSongs = 4
     tam = 70
     url = f"https://api.deezer.com/playlist/{playlist_id}/tracks?limit={tam}"
     response = requests.get(url)
     tracks = response.json()["data"]
     preview=[]
     previewUrls=[]
-    while(len(preview)<12):
-        diff = 12-len(preview)
+    while(len(preview)<numSongs):
+        diff = numSongs-len(preview)
         Aux = random.choices(tracks,k=diff)
         # Step 2: Retrieve the preview URL for each track
         for track in Aux:
             preview_track = {}
-            preview_track["id"] = track["id"]
-            preview_track["album"] = track["album"]["title"]
-            preview_track["cover_medium"] = track["album"]["cover_medium"]
             preview_track["title"] = track["title"]
-            preview_track["preview"] = track["preview"]
+            preview_track["album"] = track["album"]["title"]
+            preview_track["cover"] = track["album"]["cover_medium"]
+            preview_track["artist"] = track["artist"]["name"]
 
-            if(preview_track["preview"] !="" and not(preview_track["preview"] in previewUrls)):
-                previewUrls.append(preview_track["preview"])
+            if(track["preview"] !="" and not(track["preview"] in previewUrls)):
+                previewUrls.append(track["preview"])
                 preview.append(preview_track)
+    Aux = random.choice(previewUrls)
+    preview.append(Aux)
+    preview.append(previewUrls.index(Aux))
     # Print the previews
     return preview
 
