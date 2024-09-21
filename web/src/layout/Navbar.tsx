5af3a3eb350e8e937a2235383ca65c5c1879e7a5
Angelo Munhoz <angelomca09@gmail.com>
2024-09-21 20:38:35 -0300

    Add Link to Home Page

--- web/src/layout/Navbar.tsx
+++ web/src/layout/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { getCurrentTheme, themeToggle } from '../ts/theme-switcher'
 import { MdOutlineLightMode, MdOutlineDarkMode } from "react-icons/md"
+import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
 
@@ -9,13 +10,13 @@ export const Navbar = () => {
   return (
     <section className='mb-0'>
       <nav className="container-fluid">
-        <ul><li><a href="./" className='contrast' onClick={(e) => e.preventDefault()}><strong>GuessTheSong</strong></a></li></ul>
+        <ul><li><Link to={"/"}><strong className='contrast'>GuessTheSong</strong></Link></li></ul>
         <ul>
           <li>
-            <button className='outline secondary' onClick={(e)=>{setActualTheme(themeToggle(e))}}>
+            <button className='outline secondary' onClick={(e) => { setActualTheme(themeToggle(e)) }}>
               {actualTheme === "dark"
-                ? <MdOutlineDarkMode fontSize={"1.5rem"}/>
-                : <MdOutlineLightMode fontSize={"1.5rem"}/>
+                ? <MdOutlineDarkMode fontSize={"1.5rem"} />
+                : <MdOutlineLightMode fontSize={"1.5rem"} />
               }
             </button>
           </li>
