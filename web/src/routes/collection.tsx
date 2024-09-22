760cd1d751b21abe4f0b89a2cd5c1099015dd31c
Angelo Munhoz <angelomca09@gmail.com>
2024-09-22 01:12:50 -0300

    Apply dependency in PlayButton

--- web/src/routes/collection.tsx
+++ web/src/routes/collection.tsx
@@ -3,7 +3,7 @@ import { getGameByPlaylistId } from "../api"
 import { IGameContent } from "../interfaces/IGameContent";
 import { MusicCard } from "../layout/MusicCard";
 import { PlayButton } from "../layout/PlayButton";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const gamesLoader = async ({ params }: any) => {
     const games = await getGameByPlaylistId(params.playlistId)
@@ -12,7 +12,7 @@ export const gamesLoader = async ({ params }: any) => {
 
 export function GameColumn({ game }: { game: IGameContent }) {
 
-    const { answer, preview, tracks } = game
+    const { answer, tracks } = game
 
     const [answered, setAnswered] = useState(false)
 
@@ -31,24 +31,55 @@ export function GameColumn({ game }: { game: IGameContent }) {
     }
 
     return (
-        <div className="w-fit flex flex-col items-center select-none">
-            <PlayButton url={preview} />
-
-            <div>
-                {!!tracks.length && tracks.map((track, i) => (
-                    <MusicCard key={i} track={track} answered={answered} isCorrect={answer === i} onClick={() => chooseMusicAsOption(i, answer)} />
-                ))}
-            </div>
+        <div>
+            {!!tracks.length && tracks.map((track, i) => (
+                <MusicCard key={i} track={track} answered={answered} isCorrect={answer === i}
+                    onClick={() => chooseMusicAsOption(i, answer)} />
+            ))}
         </div>
+
     )
 }
 
 export default function CollectionRoute() {
     const { games } = useLoaderData() as { games: IGameContent[] }
 
+    const [currentSrc, setCurrentSrc] = useState("")
+
+    const audioTag = useRef(null)
+
+    const handleChange = (newPreview: string) => {
+        (audioTag.current as any)?.pause();
+        (audioTag.current as any)?.load();
+
+        if (newPreview === (audioTag.current as any).src || !newPreview) {
+            (audioTag.current as any).src = ""
+            setCurrentSrc("")
+        }
+        else {
+            (audioTag.current as any).src = newPreview;
+            (audioTag.current as any)?.play();
+            setCurrentSrc(newPreview)
+        }
+    }
+
+    useEffect(() => {
+        const volume = 0.05; // TODO: make it configurable
+        (audioTag.current as any).volume = volume;
+    }, [])
+
     return <>
         <div className="flex flex-row justify-evenly" >
-            {!!games.length && games.map(game => <GameColumn game={game} />)}
+            <audio ref={audioTag} className="hidden" onEnded={() => { handleChange("") }}></audio>
+            {!!games.length && games.map((game, i) => {
+                const { preview } = game
+
+                return (<div key={i} className="w-fit flex flex-col items-center select-none">
+                    <PlayButton isPlaying={currentSrc === preview} handleClick={() => handleChange(preview)} />
+                    <GameColumn game={game} />
+                </div>)
+            }
+            )}
         </div>
 
     </>
