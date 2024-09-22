8ddb3fd3c7478dbe7f0b804294a61b35f91666cc
Angelo Munhoz <angelomca09@gmail.com>
2024-09-21 21:12:58 -0300

    Adds more games to API response

--- server/python.py
+++ server/python.py
@@ -3,43 +3,55 @@ import random
 
 def main(playlist_id):
     # Step 1: Retrieve the tracks in the playlist
-    numSongs = 4
-    tam = 70
-    url = f"https://api.deezer.com/playlist/{playlist_id}/tracks?limit={tam}"
+    NUMGAMES = 3
+    NUMSONGS = 4
+    TAM = 70
+    url = f"https://api.deezer.com/playlist/{playlist_id}/tracks?limit={TAM}"
     response = requests.get(url)
     playlist_tracks = response.json()["data"]
-    tracks=[]
-    previews=[]
 
-    # Step 2: Fill the track list with random songs from the Playlist
-    while(len(tracks)<numSongs):
-        diff = numSongs-len(tracks)
-        random_tracks = random.choices(playlist_tracks,k=diff)
-        # Step 2.1: Retrieve information for each track
-        for track in random_tracks:
-            new_track = {}
-            new_track["title"] = track["title"]
-            new_track["album"] = track["album"]["title"]
-            new_track["cover"] = track["album"]["cover_medium"]
-            new_track["artist"] = track["artist"]["name"]
+    games = []
+    answers = []
 
-            # Step 2.2: Save track if not repeated and store it preview separately for later
-            if(track["preview"] !="" and not(track["preview"] in previews)):
-                previews.append(track["preview"])
-                tracks.append(new_track)
+    # Step 2: For the every game, build an gameObject
+    for _ in range(0,NUMGAMES):
+        previews=[]
+        tracks=[]
+        
+        # Step 3: Fill the track list with random songs from the Playlist
+        while(len(tracks)<NUMSONGS):
+            diff = NUMSONGS-len(tracks)
+            random_tracks = random.choices(playlist_tracks,k=diff)
+            # Step 3.1: Retrieve information for each track
+            for track in random_tracks:
+                new_track = {}
+                new_track["title"] = track["title"]
+                new_track["album"] = track["album"]["title"]
+                new_track["cover"] = track["album"]["cover_medium"]
+                new_track["artist"] = track["artist"]["name"]
 
-    # Step 3: Select a random preview
-    selected_preview = random.choice(previews)
+                # Step 3.2: Save track if not repeated and store it preview separately for later
+                if(track["preview"] !="" and not(track["preview"] in previews)):
+                    previews.append(track["preview"])
+                    tracks.append(new_track)
 
-    # Step 4: Build the final object
+        # Step 4: Select a random preview that was not yet selected
+        selected = False
+        while(not selected):
+            selected_preview = random.choice(previews)
+            if(not selected_preview in answers):
+                answers.append(selected_preview)
+                selected = True
 
-    obj = {}
+        # Step 5: Build the game object
+        game = {}
+        game["answer"] = previews.index(selected_preview) # INT - The index of the tracks list for that game
+        game["preview"] = selected_preview # STR - The URL for the music preview source 
+        game["tracks"] = tracks # LIST - All the tracks for that game
+        
+        games.append(game)
 
-    obj["answer"] = previews.index(selected_preview)
-    obj["preview"] = selected_preview
-    obj["tracks"] = tracks
-
-    return obj
+    return games
 
 if __name__ == '__main__':
     # Call the main function with a default playlist ID (optional)
