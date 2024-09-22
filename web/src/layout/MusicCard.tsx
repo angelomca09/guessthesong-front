48f5d0e3a91d3ea3a6357e0e8860e35d143aa3f9
Angelo Munhoz <angelomca09@gmail.com>
2024-09-21 22:35:14 -0300

    Add new logic to answered games

--- web/src/layout/MusicCard.tsx
+++ web/src/layout/MusicCard.tsx
@@ -7,12 +7,10 @@ export function MusicCard({ track, answered, isCorrect, onClick }: { track: ITra
 
     const answeredClass = answered ? "!cursor-not-allowed" : "";
 
-    const isSelectedClass = isSelected ? "border-4" : "";
-
-    const isCorrectClass = isCorrect ? "border-green-400" : "border-red-500";
+    const borderClass = `border-4 ${isCorrect ? "border-green-400" : "border-red-500"}`;
 
     return <div title={track.title} className={`w-32  pb-6 ${answeredClass}`}>
-        <button className={`outline secondary w-fit p-0 border-2 rounded-lg overflow-hidden m-0  ${isSelectedClass} ${isSelected ? isCorrectClass : ""}`}
+        <button className={`outline secondary w-fit p-0 border-2 rounded-lg overflow-hidden m-0  ${(isSelected || (answered && isCorrect)) && borderClass}`}
             disabled={answered} onClick={() => { onClick && onClick(); setIsSelected(true) }}>
             <img className="w-32" src={track.cover} alt={track.album} />
         </button>
