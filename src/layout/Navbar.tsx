9bfd9e3bb6506cee8c4a8edbb235f69ff238c316
Angelo Munhoz <angelomca09@gmail.com>
2024-09-23 20:45:04 -0300

    Add Deezer Reference

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { getCurrentTheme, themeToggle } from '../ts/theme-switcher'
-import { MdOutlineLightMode, MdOutlineDarkMode } from "react-icons/md"
+import { MdOutlineLightMode, MdOutlineDarkMode, MdOutlineSettings } from "react-icons/md"
 import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
@@ -12,6 +12,12 @@ export const Navbar = () => {
       <nav className="container-fluid">
         <ul><li><Link to={"/"}><strong className='contrast'>GuessTheSong</strong></Link></li></ul>
         <ul>
+          <li className='text-sm'>
+            <span>data and samples from: </span>
+            <a className='text-purple-600 inline-flex w-fit gap-2' target='_blank' href="https://www.deezer.com/">
+              DEEZER <img className='h-4' src="https://e-cdn-files.dzcdn.net/cache/images/common/favicon/favicon-32x32.ed120c279a693bed3a44.png" alt="Deezer logo" />
+            </a>
+          </li>
           <li>
             <button className='outline secondary' onClick={(e) => { setActualTheme(themeToggle(e)) }}>
               {actualTheme === "dark"
