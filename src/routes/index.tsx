7130dd201080ff796dcf2902660e21a2ca4d2863
Angelo Munhoz <angelomca09@gmail.com>
2024-09-24 12:10:08 -0300

    Add spinner feedback while searching

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,8 @@ import { toggleFavoritePlaylist, getFavoritePlaylists } from "../ts/playlist-bas
 import { IPlaylist } from "../interfaces/IPlaylist";
 import { getPlaylist } from "../api";
 import Footer from "../layout/Footer";
+import { MdFavorite } from "react-icons/md";
+import { ClipLoader } from "react-spinners";
 
 export default function IndexRoute() {
 
@@ -11,12 +13,15 @@ export default function IndexRoute() {
     const [favoritePlaylists, setFavoritePlaylists] = useState<IPlaylist[]>([])
     const [searchText, setSearchText] = useState("")
     const [searchedPlaylists, setsearchedPlaylists] = useState<IPlaylist[]>([]);
+    const [isSearching, setIsSearching] = useState(false);
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
+        setIsSearching(true)
         getPlaylist(searchText).then(res => {
             setsearchedPlaylists(res)
+            setIsSearching(false)
         })
     }
 
@@ -39,8 +44,9 @@ export default function IndexRoute() {
 
 
     return <>
-        <form className="pt-8" onSubmit={handleSubmit}>
+        <form className="pt-8 relative" onSubmit={handleSubmit}>
             <input autoComplete="off" type="search" id="search" name="search" placeholder="Search a playlist" value={searchText} onChange={(e) => setSearchText(e.target.value)}></input>
+            <ClipLoader hidden={!isSearching} className="absolute right-3 top-10 w-80" color="#0ea5dd" />
         </form>
         {
             !!searchedPlaylists.length &&
@@ -57,7 +63,7 @@ export default function IndexRoute() {
                 favoritePlaylists.map(playlist => (
                     <PlaylistCard key={playlist.id} isFavorite={true} playlist={playlist} onFavoriteClick={() => handleFavoriteClick(playlist)} />
                 )) :
-                <p className="m-0">Search a playlist and Favorite it!</p>
+                <p className="m-0">Search a playlist and Favorite it! <MdFavorite className="text-red-500 inline" size={"1.2rem"} /></p>
             }
         </article>
         <Footer />
